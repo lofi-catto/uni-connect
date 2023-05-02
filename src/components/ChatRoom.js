bc7bf06f622d42f74d6bfc1279a9917f76dddee1
Khoi Nguyen <khoi.nguyen@bulknutrients.com.au>
2023-05-02 18:12:24 +1000

    message input component

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -1,34 +1,40 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import localForage from 'localforage';
-import {
-  getChatRoomRef,
-  getUserRef,
-  createMessage,
-} from 'services/firestoreUtils';
+
 import MessageList from 'components/MessageList';
+import MessageInput from 'components/MessageInput';
 
 function ChatRoom() {
+  const navigate = useNavigate();
   const { chatRoomId } = useParams();
-  const [newMessageText, setNewMessageText] = useState('');
+  const [userId, setUserId] = useState(false);
 
-  const handleNewMessageSubmit = async (event) => {
-    event.preventDefault();
-
-    // Get a reference to the chat room
-    const chatRoomRef = getChatRoomRef(chatRoomId);
-
+  useEffect(() => {
     // get current user id from localforage
-    const userId = await localForage.getItem('userId');
+    const fetchUserId = async () => {
+      const userId = await localForage.getItem('userId');
+      setUserId(userId);
+    };
 
-    const userRef = getUserRef(userId);
+    fetchUserId();
+  }, []);
 
-    // Add the new message to the chat room
-    await createMessage(newMessageText, userRef, chatRoomRef);
-
-    // Clear the message input
-    setNewMessageText('');
-  };
+  if (!userId || !chatRoomId) {
+    return (
+      <div>
+        <h2>Please go back and choose a name</h2>
+        <button
+          onClick={() => {
+            navigate('/');
+          }}
+        >
+          Leave
+        </button>
+      </div>
+    );
+  }
 
   return (
     <div>
@@ -36,15 +42,7 @@ function ChatRoom() {
       <ul>
         <MessageList chatRoomId={chatRoomId} />
       </ul>
-      <form onSubmit={handleNewMessageSubmit}>
-        <input
-          type="text"
-          placeholder="Type a message"
-          value={newMessageText}
-          onChange={(event) => setNewMessageText(event.target.value)}
-        />
-        <button type="submit">Send</button>
-      </form>
+      <MessageInput chatRoomId={chatRoomId} />
     </div>
   );
 }
---
+++ src/components/MessageInput.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import localForage from 'localforage';
+import {
+  getChatRoomRef,
+  getUserRef,
+  createMessage,
+} from 'services/firestoreUtils';
+
+function MessageInput({ chatRoomId }) {
+  const [newMessageText, setNewMessageText] = useState('');
+
+  const handleChange = (event) => {
+    setNewMessageText(event.target.value);
+  };
+
+  const handleNewMessageSubmit = async (event) => {
+    event.preventDefault();
+
+    // Get a reference to the chat room
+    const chatRoomRef = getChatRoomRef(chatRoomId);
+
+    // get current user id from localforage
+    const userId = await localForage.getItem('userId');
+
+    if (!userId) {
+      return;
+    }
+
+    const userRef = getUserRef(userId);
+
+    // Add the new message to the chat room
+    await createMessage(newMessageText, userRef, chatRoomRef);
+
+    // Clear the message input
+    setNewMessageText('');
+  };
+
+  return (
+    <form onSubmit={handleNewMessageSubmit}>
+      <input
+        type="text"
+        placeholder="Enter a message"
+        value={newMessageText}
+        onChange={handleChange}
+        required
+        minLength={1}
+      />
+      <button type="submit" disabled={!newMessageText}>
+        Send
+      </button>
+    </form>
+  );
+}
+
+export default MessageInput;
